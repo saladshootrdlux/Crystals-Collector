d835f592e2581789d131da8c3f76373c635cc317
Case <38591271+saladshootrdlux@users.noreply.github.com>
2018-10-13 22:05:32 -0700

    updated some game text, changed some formatting for mobile version, added some notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,22 @@
 
+// This section is used to set the initial values within the scorebox.
+
 var targetNumber = "";
 var wins = 0;
 var losses = 0;
 var counter = 0;
+
+// This is the array of crystal images which will be pulled and populated during the resetCrystals function.
+
 var images = ["./assets/images/blue-crystal.png", "./assets/images/purple-crystal.png", "./assets/images/red-crystal.png", "./assets/images/yellow-crystal.png"];
 
+// This function sets the initial target number for the user to match using crystals.
 
 function randomTargetNumber () {
-    targetNumber = Math.floor(Math.random() * 102) + 19;
+    targetNumber = Math.floor(Math.random() * 55) + 32;
 }
+
+// This function creates the Crystal images, sets their class, grabs images from assets/images, sets randomized values to each, sets size limit, and places them in index.html.
 
 function resetCrystals () {
     for (var i = 0; i < images.length; i++) {
@@ -21,6 +29,8 @@ function resetCrystals () {
     }
 }
 
+// This function resets the HTML counter.
+
 function resetHTML () {
     $(".target-number").html(targetNumber);
     $(".win-counter").html("<p>Wins: " + wins + "</p>");
@@ -29,6 +39,8 @@ function resetHTML () {
     $(".crystal-images").empty();
 }
 
+// This combines the resets and sets a new target number when triggered.
+
 function fullReset () {
     randomTargetNumber ();
     counter = 0;
@@ -36,10 +48,15 @@ function fullReset () {
     resetCrystals ();
 }
 
+// All resets are triggered during the initial page load to prepare for the first game.
 
 randomTargetNumber();
 resetHTML ();
 resetCrystals ();
+
+// These functions are responsible for the actual game mechanics and control what happens when crystals are clicked.
+// If the user matches the number, an alert is raised "You win!".
+// If the user goes over the target numer, an alert is raised "You lose!".
 
 
 function crystalClick () {
@@ -57,63 +74,6 @@ function crystalClick () {
     };
 };
 
+// This is the global event listener which is listening for mouse clicks on crystals.
+
 $(document).on("click", ".crystal", crystalClick);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// BELOW IS COMMENTED OUT CODE FROM THE CRYSTALS GAME FOR REFERENCE ONLY
-// var targetNumber = 53;
-
-// $("#number-to-guess").text(targetNumber);
-
-// function random100() {
-//     var x = document.getElementById("demo")
-//     x.innerHTML = Math.floor((Math.random() * 100) + 1);
-// }
-
-// var counter = 0;
-// var numberOptions = [10, 5, 3, 7];
-
-// // For loop to create crystals for every numberOption.
-// for (var i = 0; i < numberOptions.length; i++) {
-
-//   var imageCrystal = $("<img>");
-
-//   imageCrystal.addClass("crystal-image");
-
-//   imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-//   imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-//   $("#crystals").append(imageCrystal);
-// }
-
-
-// $(".crystal-image").on("click", function() {
-
-//   var crystalValue = ($(this).attr("data-crystalvalue"));
-//   crystalValue = parseInt(crystalValue);
-//   counter += crystalValue;
-
-//   alert("New score: " + counter);
-
-//   if (counter === targetNumber) {
-//     alert("You win!");
-//   }
-
-//   else if (counter >= targetNumber) {
-//     alert("You lose!!");
-//   }
-
-// });
